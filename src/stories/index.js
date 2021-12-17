dd883f780a57b1e2c20c302091c9fd413fec758a
Camila Naomi <camila.naomi@credihome.com.br>
2021-12-17 16:15:35 -0300

    Testes

---
+++ src/stories/index.js
@@ -0,0 +1,11 @@
+import Button from './Button/Button';
+import Header from './Header/Header';
+import Page from './Page/Page';
+import Tokens from './Tokens/tokens.css';
+
+export {
+  Button,
+  Header,
+  Page,
+  Tokens
+}
